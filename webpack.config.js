e7186f6d706e3dd468a08bc2caa67ab0fd9a1881
Jonathan <sunyang713@hotmail.com>
2015-12-24 12:22:46 -0500

    Init webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  path = require('path'),
+  srcPath = path.join(__dirname, 'src');
+
+module.exports = {
+  target: 'web',
+  cache: true,
+  entry: {
+    common: ['react', 'react-dom', 'react-router'],
+    index: path.join(srcPath, 'index.js')
+  },
+  resolve: {
+    root: srcPath,
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules', 'src']
+  },
+  output: {
+    path: path.join(__dirname, 'cal/dist'),
+    publicPath: '',
+    filename: '[name].js',
+    library: ['Example', '[name]'],
+    pathInfo: true
+  },
+  module: {
+    loaders: [
+      // required for react jsx
+      { test: /\.js$/, exclude: /(node_modules)/, loader: "babel-loader" },
+      { test: /\.jsx$/, exclude: /(node_modules)/, loader: "babel-loader" },
+
+      // Stylus loader
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+
+      // required to write "require('./style.css')"
+      { test: /\.css$/, exclude: /\.useable\.css$/, loader: "style-loader!css-loader" },
+
+
+      // required for bootstrap icons.
+      // the url-loader uses DataUrls. 
+      // the file-loader emits files. 
+      { test: /\.(woff|woff2)$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader?prefix=font/" },
+      { test: /\.eot$/, loader: "file-loader?prefix=font/" },
+      { test: /\.svg$/, loader: "file-loader?prefix=font/" },
+
+      // misc
+      { test: /\.json$/, loader: "json-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'cal/templates/index.html'
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+    })
+  ],
+  node: {
+    net: "empty",
+    tls: "empty"
+  },
+  debug: true,
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './cal/dist',
+    historyApiFallback: true
+  }
+};
