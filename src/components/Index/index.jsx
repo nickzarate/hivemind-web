2afcdb93f48540586ddac6acabd45cd9a1680e86
Jonathan <sunyang713@hotmail.com>
2015-12-30 00:43:57 -0500

    Rename index page from 'Main' to 'Index.' Delete ReduxTest.

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 // import ParseReact from 'parse-react'
 // import ParseDump from 'components/ParseDump'
 
-export default class Main extends React.Component {
-  displayName: 'Main'
-  
+export default class Index extends React.Component {  
   render() {
     return (
       <div>
-        <h1>{ 'Front Page!!' }</h1>
-
+        <h1>{ 'Home' }</h1>
       </div>
     )
   }
