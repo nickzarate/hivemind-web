833a03bb030b35885d1a70d2fd7bbc69a2612981
Nick <nicholas.zarate@gmail.com>
2016-01-04 20:19:26 -0500

    fix round reducer

--- src/reducers/round.js
+++ src/reducers/round.js
@@ -1,14 +1,68 @@
-import { INCREMENT_CURRENT_QUESTION } from 'constants'
+import { INCREMENT_CURRENT_QUESTION, INCREMENT_NUM_QUESTIONS, DECREMENT_NUM_QUESTIONS,
+  QUESTIONS_PER_ROUND, DEFAULT_QUESTION_TYPE, SET_QUESTION_TYPE, ADD_ESTIMATE, ADD_ANSWERS } from 'constants'
 
 const initialState = {
-  currentQuestion: 1
+  currentQuestion: 1,
+  numQuestions: QUESTIONS_PER_ROUND,
+  questionType: DEFAULT_QUESTION_TYPE,
+  estimateVector: [],
+  answersVector: [],
+  idArray: []
 }
 
 export default function round(state = initialState, action) {
   switch (action.type) {
   case INCREMENT_CURRENT_QUESTION:
     return {
-      currentQuestion: state.currentQuestion + 1
+      currentQuestion: state.currentQuestion + 1,
+      numQuestions: state.numQuestions,
+      questionType: state.questionType,
+      estimateVector: state.estimateVector,
+      answersVector: state.answersVector
+    }
+  case INCREMENT_NUM_QUESTIONS:
+    return {
+      currentQuestion: state.currentQuestion,
+      numQuestions: state.numQuestions + 1,
+      questionType: state.questionType,
+      estimateVector: state.estimateVector,
+      answersVector: state.answersVector
+    }
+  case DECREMENT_NUM_QUESTIONS:
+    return {
+      currentQuestion: state.currentQuestion,
+      numQuestions: state.numQuestions - 1,
+      questionType: state.questionType,
+      estimateVector: state.estimateVector,
+      answersVector: state.answersVector
+    }
+  case SET_QUESTION_TYPE:
+    return {
+      currentQuestion: state.currentQuestion,
+      numQuestions: state.numQuestions,
+      questionType: action.questionType,
+      estimateVector: state.estimateVector,
+      answersVector: state.answersVector
+    }
+  case ADD_ESTIMATE:
+    var estimateVector = state.estimateVector
+    estimateVector.push(action.estimate)
+    return {
+      currentQuestion: state.currentQuestion,
+      numQuestions: state.numQuestions,
+      questionType: state.questionType,
+      estimateVector: estimateVector,
+      answersVector: state.answersVector
+    }
+  case ADD_ANSWERS:
+    var answersVector = state.answersVector
+    answersVector.push(action.answers)
+    return {
+      currentQuestion: state.currentQuestion,
+      numQuestions: state.numQuestions,
+      questionType: state.questionType,
+      estimateVector: state.estimateVector,
+      answersVector: answersVector
     }
   default:
     return state
