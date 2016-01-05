485576912984d4e7205cb4d1994b5590a111c566
Nick <nicholas.zarate@gmail.com>
2016-01-04 20:08:54 -0500

    add stats page

---
+++ src/components/Stats/index.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+import { roundActions } from 'actions'
+import { reduxify } from 'toolbox'
+
+class Stats extends React.Component {
+  pushPath(path) {
+    return () => this.props.pushPath(path)
+  }
+
+  render() {
+    console.log(this)
+    return (
+      <div>
+        <p>{ 'Stats page!!' }</p>
+      </div>
+    )
+  }
+}
+
+Stats.propTypes = {
+  actions: PropTypes.object.isRequired,
+  pushPath: PropTypes.func.isRequired,
+  round: PropTypes.object.isRequired
+}
+
+export default reduxify({
+  component: Stats,
+  reducer: 'round',
+  actions: roundActions
+})
