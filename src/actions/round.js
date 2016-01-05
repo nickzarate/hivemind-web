fd0aea2c493502ae74d95f8d9da11381b3785f9a
Nick <nicholas.zarate@gmail.com>
2016-01-04 20:11:47 -0500

    fix round actions

--- src/actions/round.js
+++ src/actions/round.js
@@ -1,7 +1,21 @@
-import { INCREMENT_CURRENT_QUESTION } from 'constants'
+import { INCREMENT_CURRENT_QUESTION, ADD_ESTIMATE, ADD_ANSWERS } from 'constants'
 
 export function incrementCurrentQuestion() {
   return {
     type: INCREMENT_CURRENT_QUESTION
+  }
+}
+
+export function addEstimate(estimate) {
+  return {
+    type: ADD_ESTIMATE,
+    estimate: estimate
+  }
+}
+
+export function addAnswers(answers) {
+  return {
+    type: ADD_ANSWERS,
+    answers: answers
   }
 }
