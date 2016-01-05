0ece6dac96891654c0b489f9083c5d697308c5b4
Nick <nicholas.zarate@gmail.com>
2016-01-04 20:15:04 -0500

    create question component to share common code of individual questions

--- src/components/ParseDump/index.jsx
+++ src/components/ParseDump/index.jsx
@@ -7,22 +7,10 @@ import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
 export default class ParseDump extends React.Component {
 
   componentWillMount() {
-    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
   }
 
   componentDidMount() {
-    var observationID = 1 + Math.floor(Math.random() * 99)
-    var HealthQuestion = Parse.Object.extend('HealthQuestion')
-    var query = new Parse.Query(HealthQuestion)
-    query.equalTo('observationID', observationID)
-    query.first({
-      success(response) {
-        return response
-      },
-      error(error) {
-        return error
-      }
-    })
+    
   }
 
   render() {
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,23 +1,27 @@
 import React, { PropTypes } from 'react'
-import { questionActions } from 'actions'
-import { reduxify } from 'toolbox'
 
 export default class Question extends React.Component {
-  constructor(props) {
-    super(props)
+
+  handleDeposit(index) {
+    return () => {
+      if (this.props.question.bank) {
+        this.props.actions.withdraw()
+        this.props.actions.deposit(index)
+      }
+    }
   }
 
-  placeCube(index) {
-    this.props.question.withdraw
-    this.props.question.deposit(index)
-  }
+  handleEstimate = (event) => {
+    let estimate = Number(event.target.value)
+    this.props.actions.estimate(estimate)
+  };
 
   pushPath(path) {
     return () => this.props.pushPath(path)
   }
 
   render() {
-    console.log(this)
+    //console.log(this)
     return (
       <div>
         <p>{ 'Hello!' }</p>
@@ -28,6 +32,9 @@ export default class Question extends React.Component {
 
 Question.propTypes = {
   actions: PropTypes.object.isRequired,
+  onEstimate: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
   pushPath: PropTypes.func.isRequired,
-  question: PropTypes.object.isRequired
+  question: PropTypes.object.isRequired,
+  questionType: PropTypes.string.isRequired
 }
