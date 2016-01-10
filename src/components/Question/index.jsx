447cf942e6bd5ebf36fb3d2d88b2270e6e735a21
Nick <nicholas.zarate@gmail.com>
2016-01-09 23:44:21 -0500

    Move shared functions out of EducationQuestion and into abstract Question

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,11 +1,30 @@
 import React, { PropTypes } from 'react'
+import Parse from 'parse'
+import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
 
 export default class Question extends React.Component {
+
+  componentWillMount() {
+    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
+    this.props.actions.pullQuestion(Parse, this.props.questionType + 'Question')
+  }
 
   handleDeposit(index) { return () => this.props.actions.handleDeposit(index) }
 
   pushPath(path) {
     return () => this.props.pushPath(path)
+  }
+
+  handleSubmit(answers, estimate) {
+    return () => {
+      const { actions } = this.props
+      this.props.onSubmit(answers, estimate, Parse)
+      if (!this.props.isSubmitting) {
+        this.refs.estimateInput.value = ''
+        actions.resetBank()
+        actions.pullQuestion(Parse, this.props.questionType + 'Question')
+      }
+    }
   }
 
   render() {
@@ -19,6 +38,7 @@ export default class Question extends React.Component {
 
 Question.propTypes = {
   actions: PropTypes.object.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
   onEstimate: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   pushPath: PropTypes.func.isRequired,
