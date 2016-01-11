39c801e9796386b0471ec0bb16f9b1aaff07db02
Jonathan <sunyang713@hotmail.com>
2016-01-11 15:31:02 -0500

    Remove some unnecessary eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/*eslint-env node*/
 module.exports = {
   "parser": "babel-eslint",
   "rules": {
@@ -40,7 +41,7 @@ module.exports = {
     "react/jsx-key": 1,
     "react/jsx-max-props-per-line": [
       1,
-      {"maximum": 3}
+      { "maximum": 3 }
     ],
     "react/jsx-no-bind": 1,
     "react/jsx-no-duplicate-props": 1,
@@ -48,7 +49,7 @@ module.exports = {
     "react/jsx-no-undef": 1,
     "react/jsx-pascal-case": 1,
     "react/jsx-sort-prop-types": 1,
-    "react/jsx-sort-props": 1,
+    "react/jsx-sort-props": 0,
     "react/jsx-uses-react": 1,
     "react/jsx-uses-vars": 1,
     "react/no-danger": 1,
@@ -62,7 +63,7 @@ module.exports = {
     "react/no-string-refs": 1,
     "react/no-unknown-property": 1,
     "react/prefer-es6-class": 1,
-    "react/prop-types": 1,
+    "react/prop-types": 0,
     "react/react-in-jsx-scope": 1,
     "react/require-extension": 1,
     "react/self-closing-comp": 1,
