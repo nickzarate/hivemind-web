8793926547cc1368c1353878888763b495b7386a
Nick <nicholas.zarate@gmail.com>
2016-01-13 22:15:58 -0500

    Add react-d3 and add linechart to stats component

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+let LineChart = require('react-d3/linechart').LineChart
 
 export default class Stats extends React.Component {
   pushPath(path) {
@@ -9,6 +10,13 @@ export default class Stats extends React.Component {
     return (
       <div>
         <p>{ 'Stats page!!' }</p>
+        <LineChart
+          legend={ true }
+          data={ this.props.stats.lineData }
+          width={ 500 }
+          height={ 300 }
+          title="Line Chart"
+        />
         <button onClick={ this.pushPath('/home') }>{ 'Go Home' }</button>
       </div>
     )
