896bb4a9d7d6405cd5f10fc554744800ecd86b62
Nick <nicholas.zarate@gmail.com>
2016-01-14 23:31:17 -0500

    create stats reducer to handle actions

--- src/reducers/stats.js
+++ src/reducers/stats.js
@@ -1,14 +1,30 @@
-import { SET_LINE_DATA } from 'constants'
+import { SET_COVARIATE_DATA, SET_DATA, SET_PHI } from 'constants'
 
 const initialState = {
-  lineData: []
+  data: [],
+  covariateData: [],
+  phi: []
 }
 
 export default function survey(state = initialState, action) {
   switch (action.type) {
-  case SET_LINE_DATA:
+  case SET_DATA:
     return {
-      lineData: action.payload.lineData
+      data: action.payload.data,
+      covariateData: state.covariateData,
+      phi: state.phi
+    }
+  case SET_COVARIATE_DATA:
+    return {
+      data: state.data,
+      covariateData: action.payload.covariateData,
+      phi: state.phi
+    }
+  case SET_PHI:
+    return {
+      data: state.data,
+      covariateData: state.covariateData,
+      phi: action.payload.phi
     }
   default:
     return state
