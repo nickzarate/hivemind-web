9adb4c69059d5c38aabef427eaa767a4842a8e2c
Nick <nicholas.zarate@gmail.com>
2016-01-22 11:56:00 -0500

    add test chartist comp

---
+++ src/components/Stats/Chartist.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ChartistGraph from 'react-chartist'
+
+export default class Graph extends React.Component {
+  render() {
+    var data = {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      series: [
+        [12, 9, 7, 8, 5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6]
+      ]
+    }
+
+    var options = {
+      high: 15,
+      low: 0,
+      showArea: false,
+      showPoint: true
+    }
+
+    var type = 'Line'
+
+    return (
+      <div>
+        <ChartistGraph data={data} type={type} />
+      </div>
+    )
+  }
+}
