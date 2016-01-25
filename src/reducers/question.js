467b15202ebd9b350736a68645b4ef2c15b8d7ba
Nick <nicholas.zarate@gmail.com>
2016-01-25 13:42:32 -0500

    take immutable out of question reducer

--- src/reducers/question.js
+++ src/reducers/question.js
@@ -1,24 +1,24 @@
 import { DEPOSIT, WITHDRAW, SET_CURRENT_QUESTION, SET_POINT_ESTIMATE, RESET_BANK } from 'constants'
 import default_question_config from 'assets/default_question_config.json'
-import { fromJS, List } from 'immutable'
+import update from 'react-addons-update'
 
-const initialState = fromJS({
+const initialState = {
   bank: default_question_config.NUM_CUBES,
-  bins: List(Array(default_question_config.NUM_BINS).fill(0)),
+  bins: Array(default_question_config.NUM_BINS).fill(0),
   pointEstimate: 0,
   currentQuestion: null
-})
+}
 
 export default function question(state = initialState, action) {
   switch (action.type) {
   case WITHDRAW:
-    return state.set('bank', state.get('bank') - 1)
+    return update(state, {bank: {$set: state.bank - 1}})//state.set('bank', state.get('bank') - 1)
   case DEPOSIT:
-    return state.update('bins', array => array.update(action.payload.index, value => value + 1))
+    return update(state, {bins: {$splice: [[action.payload.index, 1, state.bin[action.payload.index] + 1]]}})//state.update('bins', array => array.update(action.payload.index, value => value + 1))
   case SET_CURRENT_QUESTION:
-    return state.set('currentQuestion', action.payload.currentQuestion)
+    return update(state, {currentQuestion: {$set: action.payload.currentQuestion}})//state.set('currentQuestion', action.payload.currentQuestion)
   case SET_POINT_ESTIMATE:
-    return state.set('pointEstimate', action.payload.pointEstimate)
+    return update(state, {pointEstimate: {$set: action.payload.pointEstimate}})//state.set('pointEstimate', action.payload.pointEstimate)
   case RESET_BANK:
     return initialState
   default:
