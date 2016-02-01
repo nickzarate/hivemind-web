349809be6136fce7546b6f8d1d634237ddbeebbe
Nick <nicholas.zarate@gmail.com>
2016-02-01 15:53:12 -0500

    if there is a current user logged in, go home

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,10 +1,12 @@
 import Parse from 'parse'
 import React from 'react'
+import LoginForm from './LoginForm'
 import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
 
 export default class Login extends React.Component {
   componentWillMount() {
     Parse.initialize(APP_ID, JAVASCRIPT_KEY)
+    if (Parse.User.current()) { this.props.push('/home') }
   }
 
   push(path) {
@@ -13,25 +15,13 @@ export default class Login extends React.Component {
 
   handleEmail = (event) => this.props.actions.setUserEmail(event.target.value);
   handlePassword = (event) => this.props.actions.setUserPassword(event.target.value);
-  handleLogin = () => this.props.actions.asyncLogin(Parse, this.push('/home'));
+  handleLogin = (data) => this.props.actions.asyncLogin(Parse, this.push('/home'), data);
 
   render() {
     return (
       <div>
         <h1>{ 'Login' }</h1>
-        <form onSubmit={ this.handleLogin }>
-          <input
-            onChange={ this.handleEmail }
-            placeholder="Email"
-            type="email"
-          />
-          <input
-            onChange={ this.handlePassword }
-            placeholder="Password"
-            type="password"
-          />
-          <button onClick={ this.handleLogin }>{ 'Log In' }</button>
-        </form>
+        <LoginForm onSubmit={ this.handleLogin } />
         <button onClick={ this.push('/home') }>{ 'Home' }</button>
         <button onClick={ this.push('/signup') }>{ 'Sign Up' }</button>
       </div>
