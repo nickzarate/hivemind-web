5f5728afa403b62aa197b07c021aefc8405cbcb0
Nick <nicholas.zarate@gmail.com>
2016-02-08 15:52:59 -0500

    add range form to accept user input on their desired ranges for the round/first question

---
+++ src/components/Home/RangeForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Form from 'components/Lib/Form'
+
+export default class RangeForm extends React.Component {
+  render() {
+    return (
+      <Form onSubmit={ this.props.onSubmit }>
+        <input
+          placeholder="Lower bound"
+          type="number"
+        />
+        <input
+          placeholder="Upper bound"
+          type="number"
+        />
+        <input type="submit" value="Proceed" />
+      </Form>
+    )
+  }
+}
