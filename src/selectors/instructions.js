18296867bcf58c82ccf88ddc18093126e1dac4f8
Nick <nicholas.zarate@gmail.com>
2016-02-09 21:55:45 -0500

    Add instructions selector

---
+++ src/selectors/instructions.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect'
+
+const instructionsSelector = (state) => state.round.currentCategory
+const covariateValuesSelector = (state) => state.question.currentQuestion
+
+export default createSelector(
+  covariateValuesSelector,
+  instructionsSelector,
+  (currentQuestion, currentCategory) => {
+    return {
+      covariateValues: currentQuestion ? currentQuestion.get('covariateValues') : [],
+      covariateNames: currentCategory ? currentCategory.get('covariateNames') : [],
+      instructions: currentCategory ? currentCategory.get('instructions') : '',
+      categoryName: currentCategory ? currentCategory.get('name') : ''
+    }
+  }
+)
