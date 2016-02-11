50254ec140c74561cae9c41489b4db558afd0dc8
Nick <nicholas.zarate@gmail.com>
2016-02-10 22:07:49 -0500

    Move data handling and logic to Round container, and move submit functionality from question all into round

--- src/containers/Round.jsx
+++ src/containers/Round.jsx
@@ -1,25 +1,46 @@
 import React from 'react'
+import Parse from 'parse'
 import roundSelector from 'selectors/round'
-import * as roundActions from 'actions/round'
-import Round from 'components/Round'
+import { asyncCreateRound, asyncHandleSubmit, asyncAwardPoints } from 'actions/round'
+import { pullQuestion, reset } from 'actions/question'
 import reduxify from 'toolbox/reduxify'
+import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
 
-class RoundContainer extends React.Component {
+class Round extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
+    if (!this.props.currentCategory || !Parse.User.current()) {
+      this.props.push('/home')
+    }
+    this.props.actions.asyncCreateRound(Parse)
+    this.props.actions.pullQuestion(Parse, this.props.categoryName)
+  }
+
+  handleSubmit() {
+    const { actions, push, categoryName } = this.props
+    actions.asyncHandleSubmit(Parse, push)
+    actions.asyncAwardPoints(Parse)
+    actions.reset(this.props.bank)
+    actions.pullQuestion(Parse, categoryName)
+  }
+
   render() {
-    return(
-      <Round
-        actions={ this.props.actions }
-        push={ this.props.push }
-        round={ this.props.round }
-      >
-          { this.props.children }
-      </Round>
+    let RouteHandler = React.cloneElement(this.props.children, { onSubmit: this.handleSubmit })
+    return (
+      <div onSubmit={ this.handleSubmit }>
+        { RouteHandler }
+      </div>
     )
   }
 }
 
 export default reduxify({
   selector: roundSelector,
-  actions: roundActions,
-  container: RoundContainer
+  actions: { asyncHandleSubmit, asyncAwardPoints, asyncCreateRound, pullQuestion, reset },
+  container: Round
 })
