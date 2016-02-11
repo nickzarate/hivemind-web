53c65e5a6b405fa97344bf7273f45e732f679ca1
Nick <nicholas.zarate@gmail.com>
2016-02-10 22:08:38 -0500

    Fix question page so that submission can be handled completely by round

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import QuestionContainer from 'containers/Question'
+
+/* Containers */
 import Instructions from 'containers/Instructions'
+import Estimates from 'containers/Estimates'
+import Bins from 'containers/Bins'
 
 export default class Question extends React.Component {
   render() {
     return (
       <div>
         <Instructions />
+        <Estimates />
+        <Bins onSubmit={ this.props.onSubmit } />
       </div>
     )
   }
