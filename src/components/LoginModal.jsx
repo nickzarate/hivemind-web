44b48fbc7d49b1f283049b4834bc07889d4f9b97
Nick <nicholas.zarate@gmail.com>
2016-02-11 17:00:18 -0500

    Add a login modal component

---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+import LoginForm from 'containers/LoginForm'
+
+export default class LoginModal extends React.Component {
+  render() {
+    return (
+      <Modal show={ this.props.show } onHide={ this.props.onHide }>
+        <Modal.Header closeButton>
+          { 'Log In' }
+        </Modal.Header>
+        <Modal.Body>
+          <LoginForm />
+          <button onClick={ this.props.onSubmit }>{ 'Log In' }</button>
+        </Modal.Body>
+      </Modal>
+    )
+  }
+}
