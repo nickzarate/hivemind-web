0f40ba8730dfbf18bfacb810146bda6e04449b54
Nick <nicholas.zarate@gmail.com>
2016-02-10 22:13:20 -0500

    Move pulling of a new question out of instructions container

--- src/containers/Instructions.jsx
+++ src/containers/Instructions.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import Parse from 'parse'
-import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
-import { pullQuestion } from 'actions/question'
 import instructionsSelector from 'selectors/instructions'
 import Instructions from 'components/Instructions'
 import reduxify from 'toolbox/reduxify'
 
 class InstructionsContainer extends React.Component {
-  componentDidMount() {
-    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
-    this.props.actions.pullQuestion(Parse, this.props.categoryName)
-  }
-
   render() {
     return (
       <Instructions
@@ -25,6 +17,5 @@ class InstructionsContainer extends React.Component {
 
 export default reduxify({
   selector: instructionsSelector,
-  actions: { pullQuestion },
   container: InstructionsContainer
 })
