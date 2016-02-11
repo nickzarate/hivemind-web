7bf75204c892d28ef659881da450fa8920a25bbc
Nick <nicholas.zarate@gmail.com>
2016-02-11 14:22:12 -0500

    Create container to retrieve and set the currently available categories

---
+++ src/containers/Categories.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Parse from 'parse'
+import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
+import { handleCategoryChoice, getCategories } from 'actions/home'
+import categoriesSelector from 'selectors/categories'
+import Categories from 'components/Categories'
+import reduxify from 'toolbox/reduxify'
+
+class CategoriesContainer extends React.Component {
+  componentDidMount() {
+    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
+    if (!Parse.User.current()) {
+      this.props.push('/')
+    }
+    this.props.actions.getCategories(Parse)
+  }
+
+  handleClick = (choice) => this.props.actions.handleCategoryChoice(choice);
+
+  render() {
+    return (
+      <Categories
+        onClick={ this.handleClick }
+        categories={ this.props.categories }
+      />
+    )
+  }
+}
+
+export default reduxify({
+  selector: categoriesSelector,
+  actions: { handleCategoryChoice, getCategories },
+  container: CategoriesContainer
+})
