9a1a756648812f0baba493f522a751b68f75d3e2
Nick <nicholas.zarate@gmail.com>
2016-02-11 14:21:38 -0500

    Create container to allow the user to logout from the home page (temporary)

---
+++ src/containers/LogoutButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Parse from 'parse'
+import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
+import { clear } from 'actions/clear'
+import reduxify from 'toolbox/reduxify'
+
+class LogoutButton extends React.Component {
+  componentDidMount() {
+    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
+  }
+
+  handleClick = () => {
+    Parse.User.logOut()
+    this.props.push('/')
+  };
+
+  render() {
+    return (
+      <button onClick={ this.handleClick }>{ 'Log Out' }</button>
+    )
+  }
+}
+
+export default reduxify({
+  actions: { clear },
+  container: LogoutButton
+})
