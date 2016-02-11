72a0b5fb360935c3858a7d6087d6860b0ccc7b1e
Nick <nicholas.zarate@gmail.com>
2016-02-11 14:21:09 -0500

    Create container for the modal that receives desired user range input

---
+++ src/containers/RangeModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { handleRange, showModal } from 'actions/home'
+import rangeModalSelector from 'selectors/rangeModal'
+import RangeModal from 'components/RangeModal'
+import reduxify from 'toolbox/reduxify'
+
+class RangeModalContainer extends React.Component {
+  
+  handleSubmit = () => this.props.actions.handleRange(this.props.data, this.props.push('/round'));
+  handleHide = () => this.props.actions.showModal(false);
+
+  render() {
+    return (
+      <RangeModal
+        show={ this.props.showModal }
+        instructions={ this.props.instructions }
+        onSubmit={ this.handleSubmit }
+        onHide={ this.handleHide }
+      />
+    )
+  }
+}
+
+export default reduxify({
+  selector: rangeModalSelector,
+  actions: { handleRange, showModal },
+  container: RangeModalContainer
+})
