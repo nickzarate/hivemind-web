d3d45d84852f68fa820ece84f86e762e308d1b93
Nick <nicholas.zarate@gmail.com>
2016-02-12 11:24:09 -0500

    Tidy up estimates form using generic inputForm container

--- src/containers/EstimatesForm.jsx
+++ src/containers/EstimatesForm.jsx
@@ -1,39 +1,14 @@
 import React from 'react'
-import * as formActions from 'actions/form'
 import estimatesSelector from 'selectors/estimates'
-import InputForm from 'components/InputForm'
+import InputForm from 'containers/InputForm'
 import reduxify from 'toolbox/reduxify'
 
-class EstimatesContainer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  componentDidMount() {
-    let values = Array(this.props.placeholders.length).fill('')
-    this.props.actions.initializeValues(values)
-  }
-
-  componentWillUnmount() {
-    this.props.actions.clearValues()
-    this.props.actions.setErrorMessage(null)
-  }
-
-  handleChange () {
-    return (event, index) => {
-      this.props.actions.updateValue(event.target.value, index)
-    }
-  }
-
+class EstimatesForm extends React.Component {
   render() {
     return (
       <InputForm
-        onChange={ this.handleChange() }
         types={ this.props.types }
         placeholders={ this.props.placeholders }
-        errorMessage={ this.props.errorMessage }
-        values={ this.props.values }
       />
     )
   }
@@ -41,6 +16,5 @@ class EstimatesContainer extends React.Component {
 
 export default reduxify({
   selector: estimatesSelector,
-  actions: formActions,
-  container: EstimatesContainer
+  container: EstimatesForm
 })
