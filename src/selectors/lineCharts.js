ddf4db2d5428e359452ff2ddcced5249f7520e65
Nick <nicholas.zarate@gmail.com>
2016-02-18 12:30:43 -0500

    Add winnings to lineCharts selector so that the line charts container can show that rounds earnings

--- src/selectors/lineCharts.js
+++ src/selectors/lineCharts.js
@@ -4,17 +4,20 @@ const categorySelector = (state) => state.round.currentCategory
 const dataSelector = (state) => state.stats.data
 const indexSelector = (state) => state.stats.outcomeIndex
 const covariateDataSelector = (state) => state.stats.covariateData
+const winningsSelector = (state) => state.round.winnings
 
 export default createSelector(
   categorySelector,
   dataSelector,
   indexSelector,
   covariateDataSelector,
-  (currentCategory, data, outcomeIndex, covariateData) => {
+  winningsSelector,
+  (currentCategory, data, outcomeIndex, covariateData, winnings) => {
     return {
       data,
       outcomeIndex,
       covariateData,
+      winnings,
       covariateRanges: currentCategory ? currentCategory.get('covariateRanges') : [],
       outcomeRanges: currentCategory ? currentCategory.get('outcomeRanges') : [],
       outcomeNames: currentCategory ? currentCategory.get('outcomeNames') : []
