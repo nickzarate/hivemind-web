04038373cbeb4147240bee9744dd6ed614edfaf5
Nick <nicholas.zarate@gmail.com>
2016-02-18 16:49:56 -0500

    Alphabetize stats reducer cases

--- src/reducers/stats.js
+++ src/reducers/stats.js
@@ -2,29 +2,29 @@ import { SET_COVARIATE_DATA, SET_DATA, SET_PHI, SET_SERIES, SET_OUTCOMES, SET_OU
 import update from 'react-addons-update'
 
 const initialState = {
-  data: [],
   covariateData: [],
-  phi: [],
+  data: [],
   outcomes: [],
-  outcomeIndex: 0
+  outcomeIndex: 0,
+  phi: []
 }
 
 export default function stats(state = initialState, action) {
   switch (action.type) {
-  case SET_DATA:
-    return update(state, {data: {$set: action.payload.data}})
-  case SET_COVARIATE_DATA:
-    return update(state, {covariateData: {$set: action.payload.covariateData}})
-  case SET_PHI:
-    return update(state, {phi: {$set: action.payload.phi}})
   case ADD_PHI:
     return update(state, {phi: {$push: [action.payload.phi]}})
-  case SET_SERIES:
-    return update(state, {data: {$splice: [[action.payload.index, 1, action.payload.data]]}})
+  case SET_COVARIATE_DATA:
+    return update(state, {covariateData: {$set: action.payload.covariateData}})
+  case SET_DATA:
+    return update(state, {data: {$set: action.payload.data}})
   case SET_OUTCOMES:
     return update(state, {outcomes: {$set: action.payload.outcomes}})
   case SET_OUTCOME_INDEX:
     return update(state, {outcomeIndex: {$set: action.payload.outcomeIndex}})
+  case SET_PHI:
+    return update(state, {phi: {$set: action.payload.phi}})
+  case SET_SERIES:
+    return update(state, {data: {$splice: [[action.payload.index, 1, action.payload.data]]}})
   default:
     return state
   }
