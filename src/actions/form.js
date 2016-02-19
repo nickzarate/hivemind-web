103019de0c8beca5254de7b4cb3436a92c615ff9
Nick <nicholas.zarate@gmail.com>
2016-02-18 22:06:43 -0500

    Convert form actions to action creators

--- src/actions/form.js
+++ src/actions/form.js
@@ -1,65 +1,14 @@
 import { SET_ERROR_MESSAGE, SET_VALUE, INITIALIZE_VALUES, CLEAR_VALUES,
   CLEAR_ERRORS, INITIALIZE_ERROR_MESSAGE, SET_VALUES } from 'constants'
+import createAction from './actionCreator'
 
-export function setErrorMessage(errorMessage, formIndex) {
-  return {
-    type: SET_ERROR_MESSAGE,
-    payload: {
-      errorMessage: errorMessage,
-      formIndex: formIndex
-    }
-  }
-}
-
-export function setValue(value, formIndex, index) {
-  return {
-    type: SET_VALUE,
-    payload: {
-      value: value,
-      formIndex: formIndex,
-      index: index
-    }
-  }
-}
-
-export function initializeValues(values) {
-  return {
-    type: INITIALIZE_VALUES,
-    payload: {
-      values: values
-    }
-  }
-}
-
-export function initializeErrorMessage(errorMessage) {
-  return {
-    type: INITIALIZE_ERROR_MESSAGE,
-    payload: {
-      errorMessage: errorMessage
-    }
-  }
-}
-
-export function clearValues() {
-  return {
-    type: CLEAR_VALUES
-  }
-}
-
-export function clearErrors() {
-  return {
-    type: CLEAR_ERRORS
-  }
-}
-
-export function setValues(values) {
-  return {
-    type: SET_VALUES,
-    payload: {
-      values: values
-    }
-  }
-}
+export const setErrorMessage = createAction(SET_ERROR_MESSAGE, 'errorMessage', 'formIndex')
+export const setValue = createAction(SET_VALUE, 'value', 'formIndex', 'index')
+export const initializeValues = createAction(INITIALIZE_VALUES, 'values')
+export const initializeErrorMessage = createAction(INITIALIZE_ERROR_MESSAGE, 'errorMessage')
+export const clearValues = createAction(CLEAR_VALUES)
+export const clearErrors = createAction(CLEAR_ERRORS)
+export const setValues = createAction(SET_VALUES, 'values')
 
 export function resetValues() {
   return (dispatch, getState) => {
