7d7f8609066245675adff4bfde4db0615ad9736d
Nick <nicholas.zarate@gmail.com>
2016-02-18 23:08:33 -0500

    Alphabetize user actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,6 +1,24 @@
 import { setErrorMessage } from 'actions/form'
 
 /*
+ *  Login the user and go to the home page
+ */
+export function asyncLogin(Parse, push, formIndex = 0) {
+  return (dispatch, getState) => {
+    const { form } = getState()
+    let values = form.values[formIndex]
+    Parse.User.logIn(values[0], values[1], {
+      success() {
+        push('/home')
+      },
+      error(user, error) {
+        dispatch(setErrorMessage('Error: ' + error.code + ' ' + error.message, formIndex))
+      }
+    })
+  }
+}
+
+/*
  *  Sign up a new user with the given email and go to the home page
  *  values = [email, password1, password2]
  */
@@ -22,24 +40,6 @@ export function asyncSignup(Parse, push, formIndex = 0) {
     newUser.signUp(null, {
       success() {
         push('/survey')
-      },
-      error(user, error) {
-        dispatch(setErrorMessage('Error: ' + error.code + ' ' + error.message, formIndex))
-      }
-    })
-  }
-}
-
-/*
- *  Login the user and go to the home page
- */
-export function asyncLogin(Parse, push, formIndex = 0) {
-  return (dispatch, getState) => {
-    const { form } = getState()
-    let values = form.values[formIndex]
-    Parse.User.logIn(values[0], values[1], {
-      success() {
-        push('/home')
       },
       error(user, error) {
         dispatch(setErrorMessage('Error: ' + error.code + ' ' + error.message, formIndex))
