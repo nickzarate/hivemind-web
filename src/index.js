54f3788951cfbc423e51e0a201c4bd5cd7ac562a
Nick <nicholas.zarate@gmail.com>
2016-02-20 21:35:46 -0500

    Add redbox to index.js file in src folder

--- src/index.js
+++ src/index.js
@@ -1,52 +1,19 @@
+/* Polyfill */
+require('es6-promise').polyfill()
+
+import { createElement } from 'react'
 import { render } from 'react-dom'
 import Root from 'containers/Root'
-// import { Provider } from 'react-redux'
-// import configureStore from 'store/configureStore'
-// import { Router, Route, IndexRoute } from 'react-router'
-// import createHistory from 'history/lib/createBrowserHistory'
 
-// /* Containers */
-// import App from 'containers/App'
-// import Login from 'containers/Login'
-// import Home from 'containers/Home'
-// import Stats from 'containers/Stats'
-// import Signup from 'containers/Signup'
-// import Survey from 'containers/Survey'
-// import Question from 'containers/Question'
-// import Round from 'containers/Round'
-// import DevTools from 'containers/DevTools'
-
-// /* Components */
-// import NotFound from 'components/NotFound'
-
-// /* Styles */
-// import 'app.scss'
-
-// const history = createHistory()
-// const store = configureStore(history)
-
-// const INDEX = (
-//   <Provider store={ store }>
-//     <div>
-//       <Router history={ history }>
-//         <Route path="/" component={ App } >
-//           <IndexRoute component={ Login } />
-//           <Route path="signup" component={ Signup } />
-//           <Route path="survey" component={ Survey } />
-//           <Route path="home" component={ Home } />
-//           <Route path="round" component={ Round }>
-//             <IndexRoute component={ Question } />
-//           </Route>
-//           <Route path="stats" component={ Stats } />
-//           <Route path="*" component={ NotFound } />
-//         </Route>
-//       </Router>
-//       { IN_DEV_MODE ? <DevTools /> : null }
-//     </div>
-//   </Provider>
-// )
-
-render(
-  Root,
-  document.getElementById('content')
-)
+if (__DEV__) {
+  try {
+    render(Root, document.getElementById('root'))
+  } catch (error) {
+    render(
+      createElement(require('redbox-react'), { error }),
+      document.getElementById('root')
+    )
+  }
+} else {
+  render(Root, document.getElementById('root'))
+}
