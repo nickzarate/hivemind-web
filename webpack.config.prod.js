765945016504260c914c563a5fa33580a53f47ea
Nick <nicholas.zarate@gmail.com>
2016-02-20 20:14:02 -0500

    Add production webpack config file

---
+++ webpack.config.prod.js
@@ -0,0 +1,117 @@
+"use strict";
+
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var srcPath = path.join(__dirname, "src");
+
+
+module.exports = {
+  resolve: {
+    root: srcPath,
+    extensions: ["", ".js", ".jsx"],
+    modulesDirectories: ["node_modules", "src"]
+  },
+  entry: {
+    common: [
+      "chartist",
+      "isomorphic-fetch",
+      "jquery",
+      "parse",
+      "parse-react",
+      "rc-slider",
+      "react",
+      "react-addons-update",
+      "react-bootstrap",
+      "react-chartist",
+      "react-dom",
+      "react-redux",
+      "react-router",
+      "react-router-redux",
+      "redux",
+      "redux-thunk",
+      "reselect"
+    ],
+    "index": path.join(srcPath, "index.js")
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "",
+    filename: "js/[name]-[hash].js"
+  },
+  module: {
+    loaders: [
+      // required for react jsx
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader"
+      },
+
+      // eslint-loader
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "eslint-loader"
+      },
+
+      // Sass loader
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ["style-loader", "css", "sass"]
+      },
+
+      // Stylus loader
+      // { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+
+      // required to write "require("./style.css")"
+      {
+        test: /\.css$/,
+        exclude: /\.useable\.css$/,
+        loader: "style-loader!css-loader"
+      },
+
+      // required for bootstrap icons.
+      //    the url-loader uses DataUrls.
+      //    the file-loader emits files.
+      { test: /\.(woff|woff2)$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader?prefix=font/" },
+      { test: /\.eot$/, loader: "file-loader?prefix=font/" },
+      { test: /\.svg$/, loader: "file-loader?prefix=font/" },
+
+      // misc
+      { test: /\.json$/, loader: "json-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: path.join(srcPath, "assets/images/favicon.png"),
+      hash: true,
+      template: path.join(srcPath, "assets/index.html")
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      "__DEV__": false
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jQuery": "jquery",
+      "window.jQuery": "jquery",
+      "fetch": "isomorphic-fetch"
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "commons",
+      filename: "js/[name]-[hash].js"
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ]
+}
