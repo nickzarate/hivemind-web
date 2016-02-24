5a7c0af24acbd043159bf9ee018b9d52cf563058
Nick <nicholas.zarate@gmail.com>
2016-02-24 10:06:41 -0500

    Implement redux-actions for user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -8,7 +8,7 @@ const initialState = {
 export default function user(state = initialState, action) {
   switch (action.type) {
   case SET_ERROR_MESSAGE:
-    return update(state, {errorMessage: {$set: action.errorMessage}})
+    return update(state, {errorMessage: {$set: action.payload}})
   default:
     return state
   }
