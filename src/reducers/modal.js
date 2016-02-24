9072973996656f9c238f1dc13c3f4c6e55ac7d8a
Nick <nicholas.zarate@gmail.com>
2016-02-24 09:31:56 -0500

    Implement redux-actions in modal reducer

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -8,7 +8,7 @@ const initialState = {
 export default function question(state = initialState, action) {
   switch (action.type) {
   case SHOW_MODAL:
-    return update(state, {showModal: {$set: action.showModal}})
+    return update(state, {showModal: {$set: action.payload}})
   default:
     return state
   }
