d2beb215747f0817356880868319c8b1f80bb8bd
Nick <nicholas.zarate@gmail.com>
2016-02-24 23:16:59 -0500

    Add forms file to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 import { routeReducer as routing } from 'react-router-redux' // REMEMBER
+import { createModelReducer } from 'react-redux-form'
+import forms from './forms'
 import form from './form'
 import modal from './modal'
 import question from './question'
@@ -9,6 +11,7 @@ import survey from './survey'
 
 export default combineReducers({
   form,
+  login: createModelReducer('login', forms.initialLoginState),
   modal,
   question,
   round,
