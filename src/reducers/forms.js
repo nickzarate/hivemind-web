c6a9306cb726093e21e49232f4703466f9740d7f
Nick <nicholas.zarate@gmail.com>
2016-02-24 23:24:57 -0500

    Add forms reducer that exports all initial states of all react-redux-forms

---
+++ src/reducers/forms.js
@@ -0,0 +1,15 @@
+const initialLoginState = {
+  email: '',
+  password: ''
+}
+
+const initialSignupState = {
+  email: '',
+  password: '',
+  passwordConfirm: ''
+}
+
+export default {
+  initialLoginState,
+  initialSignupState
+}
