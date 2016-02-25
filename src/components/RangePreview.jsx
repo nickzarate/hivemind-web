0bda2318f5bedd46506872ccddb9fbd11d9a1a63
Nick <nicholas.zarate@gmail.com>
2016-02-25 11:43:41 -0500

    Add parser that turns range inputs into numbers

--- src/components/RangePreview.jsx
+++ src/components/RangePreview.jsx
@@ -3,6 +3,9 @@ import Bins from 'containers/Bins'
 import { Field } from 'react-redux-form'
 
 export default class RangePreview extends React.Component {
+
+  makeNumber = (val) => +val;
+
   renderPreview() {
     return this.props.outcomeNames.map(
       (outcomeName, index) => (
@@ -11,13 +14,13 @@ export default class RangePreview extends React.Component {
 
           { !this.props.discrete[index] &&
             <form>
-              <Field model={ `ranges.${ outcomeName }.lower` }>
+              <Field model={ `ranges.${ outcomeName }.lower` } parser={ this.makeNumber }>
                 <input
                   type="number"
                   placeholder="Lower Bound"
                 />
               </Field>
-              <Field model={ `ranges.${ outcomeName }.upper` }>
+              <Field model={ `ranges.${ outcomeName }.upper` } parser={ this.makeNumber }>
                 <input
                   type="number"
                   placeholder="Upper Bound"
