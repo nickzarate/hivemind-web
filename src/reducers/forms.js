52747ef696e482eb29a73c5650792463d8b8c561
Nick <nicholas.zarate@gmail.com>
2016-02-26 16:13:09 -0500

    Add estimates model reducer

--- src/reducers/forms.js
+++ src/reducers/forms.js
@@ -1,10 +1,12 @@
-import initialFormState from './initialFormState'
+import initialFormsState from './initialFormsState'
 import { createModelReducer, createFormReducer } from 'react-redux-form'
 import { combineReducers } from 'redux'
 
 export default combineReducers({
-  login: createModelReducer('login', initialFormState.login),
+  estimates: createModelReducer('estimates', initialFormsState.estimates),
+  estimatesForm: createFormReducer('estimates'),
+  login: createModelReducer('login', initialFormsState.login),
   loginForm: createFormReducer('login'),
-  ranges: createModelReducer('ranges', initialFormState.ranges),
+  ranges: createModelReducer('ranges', initialFormsState.ranges),
   rangesForm: createFormReducer('ranges')
 })
