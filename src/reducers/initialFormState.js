79bb4eb0ae2c885d2f6bfd8c4b53f78433146462
Nick <nicholas.zarate@gmail.com>
2016-02-25 21:41:59 -0500

    Change forms file to initialFormState as it contains the initial states for all forms

---
+++ src/reducers/initialFormState.js
@@ -0,0 +1,19 @@
+const login = {
+  email: '',
+  password: ''
+}
+
+const signup = {
+  email: '',
+  password: '',
+  passwordConfirm: ''
+}
+
+const ranges = {
+}
+
+export default {
+  login,
+  signup,
+  ranges
+}
