71e8056847e29170b9c096bdb54d13367f4bc88d
Nick <nicholas.zarate@gmail.com>
2016-02-29 10:28:19 -0500

    Delete form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import { routeReducer as routing } from 'react-router-redux' // REMEMBER
 import forms from './forms'
-import form from './form'
 import modal from './modal'
 import question from './question'
 import round from './round'
@@ -9,7 +8,6 @@ import stats from './stats'
 import survey from './survey'
 
 export default combineReducers({
-  form,
   forms,
   modal,
   question,
