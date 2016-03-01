c284c8273eb7e5b53e2e9bc15ee266005f0db2bd
Nick <nicholas.zarate@gmail.com>
2016-03-01 15:51:16 -0500

    Add refs for each Field in signup form (used for tooltip error messaging)

--- src/components/Forms/SignupForm.jsx
+++ src/components/Forms/SignupForm.jsx
@@ -3,21 +3,28 @@ import { Field } from 'react-redux-form'
 import Tooltip from 'components/Lib/Tooltip'
 
 export default class SignupForm extends React.Component {
-  form = (ref) => this.form = ref;
-
   render() {
     return (
       <div>
         <Tooltip target={ this[this.props.tooltipTarget] } message={ this.props.tooltipMessage } />
-        <form ref={ this.form } onSubmit={ this.props.onSubmit }>
-          <Field model="signup.email">
-            <input type="email" placeholder="Email" />
+        <form ref={ (ref) => this.form = ref } onSubmit={ this.props.onSubmit }>
+          <Field model="signup.email" ref={ (ref) => this.email = ref }>
+            <input
+              type="email"
+              placeholder="Email"
+            />
           </Field>
-          <Field model="signup.password">
-            <input type="password" placeholder="Password" />
+          <Field model="signup.password" ref={ (ref) => this.password = ref }>
+            <input
+              type="password"
+              placeholder="Password"
+            />
           </Field>
-          <Field model="signup.passwordConfirm">
-            <input type="password" placeholder="Confirm Password" />
+          <Field model="signup.passwordConfirm" ref={ (ref) => this.passwordConfirm = ref }>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+            />
           </Field>
           <button type="submit">{ 'Sign Up' }</button>
         </form>
