42464cdd3d29abe76946669eb32ab281269402c8
Nick <nicholas.zarate@gmail.com>
2016-03-01 11:25:11 -0500

    Take out submit button from login modal and place it in login form

--- src/containers/LoginModal.jsx
+++ src/containers/LoginModal.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import Parse from 'parse'
-import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
-import { showModal } from 'actions/modal'
-import { asyncLogin } from 'actions/user'
 import { actions } from 'react-redux-form'
-import loginModalSelector from 'selectors/loginModal'
-import LoginModal from 'components/LoginModal'
 import reduxify from 'store/reduxify'
+import LoginModal from 'components/LoginModal'
+import { showModal } from 'actions/modal'
+import loginModalSelector from 'selectors/loginModal'
 
 class LoginModalContainer extends React.Component {
-  componentDidMount() {
-    Parse.initialize(APP_ID, JAVASCRIPT_KEY)
-  }
-
   componentWillUnmount() {
     this.props.actions.showModal(false)
   }
 
-  handleSubmit = () => this.props.actions.asyncLogin(Parse, this.props.push);
   handleHide = () => {
     this.props.actions.showModal(false)
     this.props.actions.reset('login')
@@ -36,6 +28,6 @@ class LoginModalContainer extends React.Component {
 
 export default reduxify({
   selector: loginModalSelector,
-  actions: { showModal, asyncLogin, reset: actions.reset },
+  actions: { showModal, reset: actions.reset },
   container: LoginModalContainer
 })
