2e7f050497184777696df1331f6bdacd2dc509bd
Nick <nicholas.zarate@gmail.com>
2016-03-01 12:37:56 -0500

    Add a tooltip to signup form component

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -4,18 +4,16 @@ import Tooltip from 'components/Lib/Tooltip'
 
 export default class LoginForm extends React.Component {
   form = (ref) => this.form = ref;
-  email = (ref) => this.email = ref;
-  password = (ref) => this.password = ref;
 
   render() {
     return (
       <div>
         <Tooltip target={ this[this.props.tooltipTarget] } message={ this.props.tooltipMessage } />
         <form ref={ this.form } onSubmit={ this.props.onSubmit }>
-          <Field model="login.email" ref={ this.email }>
+          <Field model="login.email">
             <input type="email" placeholder="Email" />
           </Field>
-          <Field model="login.password" ref={ this.password }>
+          <Field model="login.password">
             <input type="password" placeholder="Password" />
           </Field>
           <button type="submit">{ 'Log In' }</button>
--- src/components/Forms/SignupForm.jsx
+++ src/components/Forms/SignupForm.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { Field } from 'react-redux-form'
+import Tooltip from 'components/Lib/Tooltip'
 
 export default class SignupForm extends React.Component {
+  form = (ref) => this.form = ref;
+
   render() {
     return (
-      <form>
-        <Field model="signup.email">
-          <input type="email" placeholder="Email" />
-        </Field>
-        <Field model="signup.password">
-          <input type="password" placeholder="Password" />
-        </Field>
-        <Field model="signup.passwordConfirm">
-          <input type="password" placeholder="Confirm Password" />
-        </Field>
-      </form>
+      <div>
+        <Tooltip target={ this[this.props.tooltipTarget] } message={ this.props.tooltipMessage } />
+        <form ref={ this.form } onSubmit={ this.props.onSubmit }>
+          <Field model="signup.email">
+            <input type="email" placeholder="Email" />
+          </Field>
+          <Field model="signup.password">
+            <input type="password" placeholder="Password" />
+          </Field>
+          <Field model="signup.passwordConfirm">
+            <input type="password" placeholder="Confirm Password" />
+          </Field>
+          <button type="submit">{ 'Sign Up' }</button>
+        </form>
+      </div>
     )
   }
 }
