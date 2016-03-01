4b43960d4b25c45fe7f1b2b24b7c0c347dbecc74
Nick <nicholas.zarate@gmail.com>
2016-03-01 15:48:16 -0500

    Add form validation to signup action

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,4 +1,4 @@
-import { setMessage } from './tooltip'
+import { setMessage, setTarget } from './tooltip'
 
 /*
  *  Login the user and go to the home page
@@ -20,10 +20,37 @@ export function asyncLogin(Parse, push) {
 /*
  *  Sign up a new user with the given email and go to the home page
  */
-export function asyncSignup(Parse, push) {
+export function asyncHandleSignup(Parse, push) {
   return (dispatch, getState) => {
     const { forms: { signup } } = getState()
     let newUser = new Parse.User()
+
+    // Validation
+    if (signup.email.length === 0) {
+      dispatch(setMessage('All fields must be filled.'))
+      dispatch(setTarget('email'))
+      return
+    }
+    else if (signup.password.length === 0) {
+      dispatch(setMessage('All fields must be filled.'))
+      dispatch(setTarget('password'))
+      return
+    }
+    else if (signup.passwordConfirm.length === 0) {
+      dispatch(setMessage('All fields must be filled.'))
+      dispatch(setTarget('passwordConfirm'))
+      return
+    }
+    else if (signup.password.length < 6) {
+      dispatch(setMessage('Password must be at least 6 characters long.'))
+      dispatch(setTarget('password'))
+      return
+    } else if (signup.password !== signup.passwordConfirm) {
+      dispatch(setMessage('Passwords do not match, try again.'))
+      dispatch(setTarget('passwordConfirm'))
+      return
+    }
+
     newUser.set('username', signup.email)
     newUser.set('email', signup.email)
     newUser.set('password', signup.password)
