eeaf1fd6b11ff89ff56eb71a3e3f26a2c1b1af6d
Nick <nicholas.zarate@gmail.com>
2016-03-01 11:24:06 -0500

    Add tooltip to handle error messaging in login form

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Field } from 'react-redux-form'
+import Tooltip from 'components/Lib/Tooltip'
 
 export default class LoginForm extends React.Component {
+  form = (ref) => this.form = ref;
+  email = (ref) => this.email = ref;
+  password = (ref) => this.password = ref;
+
   render() {
     return (
-      <form>
-        <Field model="login.email">
-          <input type="email" placeholder="Email" />
-        </Field>
-        <Field model="login.password">
-          <input type="password" placeholder="Password" />
-        </Field>
-      </form>
+      <div>
+        <Tooltip target={ this[this.props.tooltipTarget] } message={ this.props.tooltipMessage } />
+        <form ref={ this.form } onSubmit={ this.props.onSubmit }>
+          <Field model="login.email" ref={ this.email }>
+            <input type="email" placeholder="Email" />
+          </Field>
+          <Field model="login.password" ref={ this.password }>
+            <input type="password" placeholder="Password" />
+          </Field>
+          <button type="submit">{ 'Log In' }</button>
+        </form>
+      </div>
     )
   }
 }
