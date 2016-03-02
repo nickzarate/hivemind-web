967deec4e62771e5a415677d62c9e2b824334bb7
Nick <nicholas.zarate@gmail.com>
2016-03-01 21:15:31 -0500

    Add reset tooltip action to tooltip reducer

--- src/reducers/tooltip.js
+++ src/reducers/tooltip.js
@@ -1,4 +1,4 @@
-import { SET_TOOLTIP_TARGET, SET_TOOLTIP_MESSAGE } from 'actions/constants'
+import { RESET_TOOLTIP, SET_TOOLTIP_TARGET, SET_TOOLTIP_MESSAGE } from 'actions/constants'
 import update from 'react-addons-update'
 
 const initialState = {
@@ -8,6 +8,8 @@ const initialState = {
 
 export default function tooltip(state = initialState, action) {
   switch (action.type) {
+  case RESET_TOOLTIP:
+    return update(state, {$set: initialState})
   case SET_TOOLTIP_MESSAGE:
     return update(state, {message: {$set: action.payload}})
   case SET_TOOLTIP_TARGET:
