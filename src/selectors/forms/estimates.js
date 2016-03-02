c69ff66658cc465bdfcc3c77980db91b5470e601
Nick <nicholas.zarate@gmail.com>
2016-03-02 00:05:52 -0500

    Move estimates form selector into forms folder in the selectors folder

--- src/selectors/forms/estimates.js
+++ src/selectors/forms/estimates.js
@@ -2,14 +2,18 @@ import { createSelector } from 'reselect'
 
 const categorySelector = (state) => state.round.currentCategory
 const estimatesSelector = (state) => state.forms.estimates
+const tooltipSelector = (state) => state.tooltip
 
 export default createSelector(
   categorySelector,
   estimatesSelector,
-  (currentCategory, estimates) => {
+  tooltipSelector,
+  (currentCategory, estimates, tooltip) => {
     return {
       outcomeNames: currentCategory ? currentCategory.get('outcomeNames') : [],
-      estimates
+      estimates,
+      tooltipMessage: tooltip.message,
+      tooltipTarget: tooltip.target
     }
   }
 )
