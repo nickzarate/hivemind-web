842cc5027e1b6613fa5a3f8cdf50574a4027841c
Nick <nicholas.zarate@gmail.com>
2016-03-02 00:11:40 -0500

    Add state to the selector of the round container that allows form validation on question submission

--- src/selectors/round.js
+++ src/selectors/round.js
@@ -1,14 +1,18 @@
 import { createSelector } from 'reselect'
 
 const roundSelector = (state) => state.round.currentCategory
+const estimatesSelector = (state) => state.forms.estimates
 
 export default createSelector(
   roundSelector,
-  (currentCategory) => {
+  estimatesSelector,
+  (currentCategory, estimates) => {
     return {
       categoryName: currentCategory.get('name'),
       bank: currentCategory.get('tokens'),
-      numBins: currentCategory.get('numBins')
+      numBins: currentCategory.get('numBins'),
+      outcomeNames: currentCategory.get('outcomeNames'),
+      estimates
     }
   }
 )
