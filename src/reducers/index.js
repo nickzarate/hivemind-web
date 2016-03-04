e086ccafc4465e996321e039945ebe9ad80f165d
Nick <nicholas.zarate@gmail.com>
2016-03-04 10:59:47 -0500

    Add answer and category reducer into the reducers index.js file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 import { routeReducer as routing } from 'react-router-redux' // REMEMBER
+import answer from './answer'
+import category from './category'
 import forms from './forms'
 import modal from './modal'
 import question from './question'
@@ -9,6 +11,8 @@ import survey from './survey'
 import tooltip from './tooltip'
 
 export default combineReducers({
+  answer,
+  category,
   forms,
   modal,
   question,
