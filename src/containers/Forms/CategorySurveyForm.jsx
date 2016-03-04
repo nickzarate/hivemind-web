4a406a1c0fc02d636546dc27e51e7b9425114126
Nick <nicholas.zarate@gmail.com>
2016-03-04 11:34:34 -0500

    Move category survey form selector to forms folder

--- src/containers/Forms/CategorySurveyForm.jsx
+++ src/containers/Forms/CategorySurveyForm.jsx
@@ -3,7 +3,7 @@ import reduxify from 'store/reduxify'
 import CategorySurveyForm from 'components/Forms/CategorySurveyForm'
 import { actions } from 'react-redux-form'
 import { resetTooltip } from 'actions/tooltip'
-import categorySurveyFormSelector from 'selectors/categorySurveyForm'
+import categorySurveyFormSelector from 'selectors/forms/categorySurvey'
 
 class CategorySurveyFormContainer extends React.Component {
   componentWillUnmount() {
