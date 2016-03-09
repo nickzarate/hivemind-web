c1c9460101f827c8e8ced86bb71583e1c09c5ab2
Nick <nicholas.zarate@gmail.com>
2016-03-09 12:11:35 -0500

    Do not push to login page when the signup modal hides

--- src/containers/SignupModal.jsx
+++ src/containers/SignupModal.jsx
@@ -3,17 +3,13 @@ import { showModal } from 'actions/modal'
 import signupModalSelector from 'selectors/signupModal'
 import SignupModal from 'components/SignupModal'
 import reduxify from 'store/reduxify'
-import { browserHistory } from 'react-router'
 
 class SignupModalContainer extends React.Component {
   componentWillUnmount() {
     this.props.actions.showModal(false)
   }
 
-  handleHide = () => {
-    this.props.actions.showModal(false)
-    browserHistory.push('/')
-  };
+  handleHide = () => this.props.actions.showModal(false);
 
   render() {
     return (
