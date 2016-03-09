15c0bd909aba53d73f7631e1490bc891d76dd12d
Nick <nicholas.zarate@gmail.com>
2016-03-09 12:54:51 -0500

    Add toBool parser that turns the value into a boolean

--- src/toolbox/parser.js
+++ src/toolbox/parser.js
@@ -1,1 +1,8 @@
-export const makeNumber = (val) => { if (val !== '') { return +val } }
+export const toNum = (val) => { if (val !== '') { return +val } }
+export const toBool = (val) => {
+  if (val === 'true') {
+    return true
+  } else if (val === 'false') {
+    return false
+  }
+}
