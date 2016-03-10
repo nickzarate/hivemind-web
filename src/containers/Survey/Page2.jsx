9449417fbe36022253ca80f5c9a21dd1c5a84dd3
Nick <nicholas.zarate@gmail.com>
2016-03-09 23:43:23 -0500

    Fix selector for surveyPage2 container

--- src/containers/Survey/Page2.jsx
+++ src/containers/Survey/Page2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import reduxify from 'store/reduxify'
 import SurveyPage2 from 'components/Survey/Page2'
 import { submitPage2 } from 'actions/survey'
-import surveyPage1Selector from 'selectors/survey'
+import surveyFormSelector from 'selectors/surveyForm'
 
 class SurveyPage2Container extends React.Component {
   handleSubmit = () => this.props.actions.submitPage2();
@@ -11,14 +11,14 @@ class SurveyPage2Container extends React.Component {
     return (
       <SurveyPage2
         onSubmit={ this.handleSubmit }
-        survey={ this.props.survey }
+        surveyForm={ this.props.surveyForm }
       />
     )
   }
 }
 
 export default reduxify({
-  selector: surveyPage1Selector,
+  selector: surveyFormSelector,
   actions: { submitPage2 },
   container: SurveyPage2Container
 })
