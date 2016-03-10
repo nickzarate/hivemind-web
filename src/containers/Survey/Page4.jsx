e90c35ad8e3647e5e0a6fd36cc3dac89c8f640e3
Nick <nicholas.zarate@gmail.com>
2016-03-09 23:44:08 -0500

    Update surveyPage4 container (for checkbox)

--- src/containers/Survey/Page4.jsx
+++ src/containers/Survey/Page4.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import reduxify from 'store/reduxify'
 import SurveyPage4 from 'components/Survey/Page4'
 import { submitSurvey } from 'actions/survey'
-import surveyPage1Selector from 'selectors/survey'
+import surveySelector from 'selectors/survey'
+import { actions } from 'react-redux-form'
 
 class SurveyPage4Container extends React.Component {
   handleSubmit = () => this.props.actions.submitSurvey();
+  handleChangeValue = (val, checked) => this.props.actions.change(`survey.${ val }`, !checked);
 
   render() {
     return (
       <SurveyPage4
         onSubmit={ this.handleSubmit }
         survey={ this.props.survey }
+        changeValue={ this.handleChangeValue }
       />
     )
   }
 }
 
 export default reduxify({
-  selector: surveyPage1Selector,
-  actions: { submitSurvey },
+  selector: surveySelector,
+  actions: { submitSurvey, change: actions.change },
   container: SurveyPage4Container
 })
