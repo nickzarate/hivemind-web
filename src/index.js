035d7c2e30692e6f2fdc925d5453bb5abce93573
Nick <nicholas.zarate@gmail.com>
2016-03-11 10:39:06 -0500

    Take out redbox from ROOT

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-/* Polyfill */
-require('es6-promise').polyfill()
-
-import { createElement } from 'react'
 import { render } from 'react-dom'
 import Root from 'containers/Root'
-
-if (__DEV__) {
-  try {
-    render(Root, document.getElementById('root'))
-  } catch (error) {
-    render(
-      createElement(require('redbox-react'), { error }),
-      document.getElementById('root')
-    )
-  }
-} else {
-  render(Root, document.getElementById('root'))
-}
+render(Root, document.getElementById('root'))
