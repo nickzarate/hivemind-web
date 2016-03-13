ff52bde58cc26a21552e98dd3ae740c4781674c5
Nick <nicholas.zarate@gmail.com>
2016-03-12 21:50:12 -0500

    Fix survey actions so that each survey page validates their own questions.

--- src/actions/survey.js
+++ src/actions/survey.js
@@ -6,23 +6,23 @@ import { APP_ID, JAVASCRIPT_KEY } from 'KEYCHAIN'
 
 export function submitPage1() {
   return (dispatch, getState) => {
-    const { forms: { surveyForm } } = getState()
     // TODO: Check values of the survey form and set validity of surveyForm
     dispatch(actions.validateErrors('survey.stateOfResidence', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.gender', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.hispanic', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.isBornInUS', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.isEnglishPrimaryLanguage', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
+    const { surveyForm } = getState()
     if (surveyForm.valid) {
       browserHistory.push('/signup/2')
     }
@@ -31,26 +31,35 @@ export function submitPage1() {
 
 export function submitPage2() {
   return (dispatch, getState) => {
-    const { forms: { surveyForm } } = getState()
     // TODO: Check values of the survey form and set validity of surveyForm
     dispatch(actions.validateErrors('survey.isLunchReducedPrice', {
-      required: (val) => { console.log(val); return typeof val === 'string' && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
+    }))
+    dispatch(actions.validateErrors('survey.birthdate.day', {
+      required: (val) => typeof val === 'string' && 'Must answer!'
+    }))
+    dispatch(actions.validateErrors('survey.birthdate.month', {
+      required: (val) => typeof val === 'string' && 'Must answer!'
+    }))
+    dispatch(actions.validateErrors('survey.birthdate.year', {
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.isStudent', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.isEmployed', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.annualSalary', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.workHoursPerWeek', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.yearsOfWorkExperienceOverall', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
+    const { surveyForm } = getState()
     if (surveyForm.valid) {
       browserHistory.push('/signup/3')
     }
@@ -59,23 +68,23 @@ export function submitPage2() {
 
 export function submitPage3() {
   return (dispatch, getState) => {
-    const { forms: { surveyForm } } = getState()
     // TODO: Check values of the survey form and set validity of surveyForm
     dispatch(actions.validateErrors('survey.yearsOfWorkExperienceInCurrentCareer', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.yearsOfCompletedSchooling', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.intendedYearsOfCompletedSchooling', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.highestEducationDegree', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.intendedHighestEducationDegree', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
+    const { surveyForm } = getState()
     if (surveyForm.valid) {
       browserHistory.push('/signup/4')
     }
@@ -84,27 +93,27 @@ export function submitPage3() {
 
 export function submitSurvey() {
   return (dispatch, getState) => {
-    const { forms: { surveyForm } } = getState()
     Parse.initialize(APP_ID, JAVASCRIPT_KEY)
     // TODO: Check values of the survey form and set validity of surveyForm
     dispatch(actions.validateErrors('survey.highSchoolGPA', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.collegeGPA', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.fathersEducation', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.fathersEducationIsGuess', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.mothersEducation', {
-      required: (val) => { console.log(val); return val && val.length === 0 && 'Must answer!' }
+      required: (val) => !val.length && 'Must answer!'
     }))
     dispatch(actions.validateErrors('survey.mothersEducationIsGuess', {
-      required: (val) => { console.log(val); return typeof val === 'string' && 'Must answer!' }
+      required: (val) => typeof val === 'string' && 'Must answer!'
     }))
+    const { surveyForm } = getState()
     if (surveyForm.valid) {
       dispatch(showModal(true))
     }
