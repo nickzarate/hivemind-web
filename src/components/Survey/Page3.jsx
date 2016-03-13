f1455d78bea4de3c17f432ee339ac6747e6afe76
Nick <nicholas.zarate@gmail.com>
2016-03-12 21:52:42 -0500

    Add errors to surveyPage3 and space out each question for readability purposes

--- src/components/Survey/Page3.jsx
+++ src/components/Survey/Page3.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, createFieldClass, controls } from 'react-redux-form'
+import { Form, getField, createFieldClass, controls } from 'react-redux-form'
 import { Input } from 'react-bootstrap'
 import { toNum } from 'toolbox/parser'
 
@@ -8,23 +8,44 @@ const InputField = createFieldClass({
 })
 
 export default class SurveyPage3 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.displayErrors = this.displayErrors.bind(this)
+  }
+
+  displayErrors(state) {
+    var errors = getField(this.props.surveyForm, state).errors
+    return Object.keys(errors).map(
+      (error, index) => (
+        <p key={ index }>{ errors[error] }</p>
+      )
+    )
+  }
+
   render() {
     return (
       <Form onSubmit={ this.props.onSubmit } model="survey">
+
         <InputField model="survey.yearsOfWorkExperienceInCurrentCareer" parser={ toNum }>
           <label>{ 'How many years of work experience do you have in your current career?' }</label>
           <Input type="text" placeholder="Years" />
         </InputField>
+        { this.displayErrors('yearsOfWorkExperienceInCurrentCareer') }
+
         <InputField model="survey.yearsOfCompletedSchooling" parser={ toNum }>
           <label>{ 'Current years of completed schooling' }</label>
           <Input type="text" placeholder="Years" />
           { 'As a reference see the following...' }
         </InputField>
+        { this.displayErrors('yearsOfCompletedSchooling') }
+
         <InputField model="survey.intendedYearsOfCompletedSchooling" parser={ toNum }>
           <label>{ 'How many years of schooling do you INTEND to complete?' }</label>
           <Input type="text" placeholder="Years" />
           { 'As a reference see the following...' }
         </InputField>
+        { this.displayErrors('intendedYearsOfCompletedSchooling') }
+
         <InputField model="survey.highestEducationDegree">
           <label>{ 'What is your highest earned degree in education?' }</label>
           <Input type="radio" value="Less than a high school degree" label="Less than a high school degree" />
@@ -35,6 +56,8 @@ export default class SurveyPage3 extends React.Component {
           <Input type="radio" value="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" label="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" />
           <Input type="radio" value="Post-doctoral degree" label="Post-doctoral degree" />
         </InputField>
+        { this.displayErrors('highestEducationDegree') }
+
         <InputField model="survey.intendedHighestEducationDegree">
           <label>{ 'What is the highest degree you plan on earning?' }</label>
           <Input type="radio" value="Less than a high school degree" label="Less than a high school degree" />
@@ -45,6 +68,8 @@ export default class SurveyPage3 extends React.Component {
           <Input type="radio" value="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" label="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" />
           <Input type="radio" value="Post-doctoral degree" label="Post-doctoral degree" />
         </InputField>
+        { this.displayErrors('intendedHighestEducationDegree') }
+
         <button type="submit">{ 'Continue' }</button>
       </Form>
     )
