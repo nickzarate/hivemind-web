ea14f73df54405ac692b3e857120c4543ca37c96
Nick <nicholas.zarate@gmail.com>
2016-03-12 21:53:00 -0500

    Add errors to surveyPage4 and space out each question for readability purposes

--- src/components/Survey/Page4.jsx
+++ src/components/Survey/Page4.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, createFieldClass, controls } from 'react-redux-form'
+import { Form, getField, createFieldClass, controls } from 'react-redux-form'
 import { Input } from 'react-bootstrap'
 import { toNum } from 'toolbox/parser'
 
@@ -8,19 +8,38 @@ const InputField = createFieldClass({
 })
 
 export default class SurveyPage4 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.displayErrors = this.displayErrors.bind(this)
+  }
+
+  displayErrors(state) {
+    var errors = getField(this.props.surveyForm, state).errors
+    return Object.keys(errors).map(
+      (error, index) => (
+        <p key={ index }>{ errors[error] }</p>
+      )
+    )
+  }
+
   handleChange = (val) => { return () => this.props.onCheckChange(val) };
 
   render() {
     return (
       <Form onSubmit={ this.props.onSubmit } model="survey">
+
         <InputField model="survey.highSchoolGPA" parser={ toNum }>
           <label>{ 'What was your high school GPA? (If you do not know, give your best guess)' }</label>
           <Input type="text" placeholder="High School GPA" />
         </InputField>
+        { this.displayErrors('highSchoolGPA') }
+
         <InputField model="survey.collegeGPA" parser={ toNum }>
           <label>{ 'What was your College GPA? (If you do not know, give your best guess)' }</label>
           <Input type="text" placeholder="College GPA" />
         </InputField>
+        { this.displayErrors('collegeGPA') }
+
         <InputField model="survey.fathersEducation">
           <label>{ 'What is the highest level of education obtained by your father?' }</label>
           <Input type="radio" value="Less than a high school degree" label="Less than a high school degree" />
@@ -31,7 +50,9 @@ export default class SurveyPage4 extends React.Component {
           <Input type="radio" value="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" label="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" />
           <Input type="radio" value="Post-doctoral degree" label="Post-doctoral degree" />
         </InputField>
+        { this.displayErrors('fathersEducation') }
         <Input onChange={ this.handleChange('fathersEducationIsGuess') } type="checkbox" label="This is a guess" />
+
         <InputField model="survey.mothersEducation">
           <label>{ 'What is the highest level of education obtained by your mother?' }</label>
           <Input type="radio" value="Less than a high school degree" label="Less than a high school degree" />
@@ -42,7 +63,9 @@ export default class SurveyPage4 extends React.Component {
           <Input type="radio" value="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" label="PhD, MD (Doctor), or JD (Lawyer) (3- to 6-year graduate degree)" />
           <Input type="radio" value="Post-doctoral degree" label="Post-doctoral degree" />
         </InputField>
+        { this.displayErrors('mothersEducation') }
         <Input onChange={ this.handleChange('mothersEducationIsGuess') } type="checkbox" label="This is a guess" />
+
         <button type="submit">{ 'Continue' }</button>
       </Form>
     )
