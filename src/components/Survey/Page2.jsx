f9b689f5508cebbdc969eec6170de9f51a954769
Nick <nicholas.zarate@gmail.com>
2016-03-14 15:57:10 -0400

    For survey page 2 component, access state through forms.survey instead of survey

--- src/components/Survey/Page2.jsx
+++ src/components/Survey/Page2.jsx
@@ -28,9 +28,9 @@ export default class SurveyPage2 extends React.Component {
   render() {
     const { survey } = this.props
     return (
-      <Form onSubmit={ this.props.onSubmit } model="survey">
+      <Form onSubmit={ this.props.onSubmit } model="forms.survey">
 
-        <RadioField model="survey.isLunchReducedPrice">
+        <RadioField model="forms.survey.isLunchReducedPrice">
           <label>{ 'Have you ever received free or reduced price lunch where you attended school?' }</label>
           <Input type="radio" value="Yes" label="Yes" active />
           <Input type="radio" value="No" label="No" />
@@ -39,48 +39,48 @@ export default class SurveyPage2 extends React.Component {
         { this.displayErrors('isLunchReducedPrice') }
 
         <label>{ 'Birthdate' }</label>
-        <TextField model="survey.birthdate.month" parser={ toNum }>
+        <TextField model="forms.survey.birthdate.month" parser={ toNum }>
           <Input type="text" placeholder="MM" value={ survey.birthdate.month } />
         </TextField>
         { this.displayErrors('birthdate.month') }
 
-        <TextField model="survey.birthdate.day" parser={ toNum }>
+        <TextField model="forms.survey.birthdate.day" parser={ toNum }>
           <Input type="text" placeholder="DD" value={ survey.birthdate.day } />
         </TextField>
         { this.displayErrors('birthdate.day') }
 
-        <TextField model="survey.birthdate.year" parser={ toNum }>
+        <TextField model="forms.survey.birthdate.year" parser={ toNum }>
           <Input type="text" placeholder="YYYY" value={ survey.birthdate.year } />
         </TextField>
         { this.displayErrors('birthdate.year') }
 
-        <RadioField model="survey.isStudent">
+        <RadioField model="forms.survey.isStudent">
           <label>{ 'Are you currently a student?' }</label>
           <Input type="radio" value="Yes" label="Yes" active />
           <Input type="radio" value="No" label="No" />
         </RadioField>
         { this.displayErrors('isStudent') }
 
-        <RadioField model="survey.isEmployed">
+        <RadioField model="forms.survey.isEmployed">
           <label>{ 'Are you currently employed?' }</label>
           <Input type="radio" value="Yes" label="Yes" active />
           <Input type="radio" value="No" label="No" />
         </RadioField>
         { this.displayErrors('isEmployed') }
 
-        <TextField model="survey.annualSalary" parser={ toNum }>
+        <TextField model="forms.survey.annualSalary" parser={ toNum }>
           <label>{ 'What is your current annual salary, rounded to the nearest thousand (for example $25,325 would simply be $25,000)' }</label>
           <Input type="text" placeholder="Annual Salary" value={ survey.annualSalary } />
         </TextField>
         { this.displayErrors('annualSalary') }
 
-        <TextField model="survey.workHoursPerWeek" parser={ toNum }>
+        <TextField model="forms.survey.workHoursPerWeek" parser={ toNum }>
           <label>{ 'About how many hours per week do you work?' }</label>
           <Input type="text" placeholder="Hours" value={ survey.workHoursPerWeek } />
         </TextField>
         { this.displayErrors('workHoursPerWeek') }
 
-        <TextField model="survey.yearsOfWorkExperienceOverall" parser={ toNum }>
+        <TextField model="forms.survey.yearsOfWorkExperienceOverall" parser={ toNum }>
           <label>{ 'How many years of work experience do you have overall (counting all time employed)?' }</label>
           <Input type="text" placeholder="Years" value={ survey.yearsOfWorkExperienceOverall } />
         </TextField>
