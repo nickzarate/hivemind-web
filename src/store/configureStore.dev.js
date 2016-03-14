dddfb05510c9c909c21b613ba8bf138ad8e662c9
Nick <nicholas.zarate@gmail.com>
2016-03-14 17:31:58 -0400

    Persist custom state with new redux-localstorage

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,30 +1,34 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import reducers from 'reducers'
+import rootReducer from 'reducers'
+import persistState, { mergePersistedState } from 'redux-localstorage'
+import adapter from 'redux-localstorage/lib/adapters/localStorage'
+import filter from 'redux-localstorage-filter'
 import DevTools from 'containers/DevTools'
-import persistState from 'redux-localstorage'
-// import configSlicer from './configSlicer'
 
-export default function configureStore(initialState) {
-  // const localStorageConfig = {
-  //   slicer: configSlicer
-  // }
+export default function configureStore() {
+  const reducers = compose(
+    mergePersistedState()
+  )(rootReducer)
+
+  const storage = compose(
+    filter(['answer', 'category', 'question', 'forms.survey'])
+  )(adapter(window.localStorage))
 
   const store = createStore(
     reducers,
-    initialState,
     compose(
       applyMiddleware(thunk),
-      persistState(['answer', 'category', 'question', 'survey']),//, localStorageConfig),
+      persistState(storage),
       DevTools.instrument()
     )
   )
 
   // Enable Webpack hot module replacement for reducers
-  if (module.hot)
-    module.hot.accept('reducers', () =>
-      store.replaceReducer(require('reducers').default)
-    )
+  // if (module.hot)
+  //   module.hot.accept('reducers', () =>
+  //     store.replaceReducer(require('reducers').default)
+  //   )
 
   return store
 }
