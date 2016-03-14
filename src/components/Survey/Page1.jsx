4d472d1a032beba050a2ef1afbf2f8dfc7ca24ab
Nick <nicholas.zarate@gmail.com>
2016-03-14 12:33:54 -0400

    Display current selected values stored in state on surveyPage1

--- src/components/Survey/Page1.jsx
+++ src/components/Survey/Page1.jsx
@@ -30,26 +30,27 @@ export default class SurveyPage1 extends React.Component {
   handleChange = (val) => { return () => this.props.onCheckChange(val) };
 
   render() {
+    const { race } = this.props.survey
     return (
       <Form onSubmit={ this.props.onSubmit } model="survey">
 
         <SelectField model="survey.stateOfResidence">
-          <Input type="select" label="What state do you live in?">
-            { survey.STATE_OPTIONS.map( (option) => ( <option key={ option }>{ option }</option> ) ) }
+          <Input type="select" label="What state do you live in?" defaultValue={ race.stateOfResidence }>
+            { survey.STATE_OPTIONS.map( (option) => ( <option key={ option } value={ option }>{ option }</option> ) ) }
           </Input>
         </SelectField>
         { this.displayErrors('stateOfResidence') }
 
         <RadioField model="survey.gender">
           <label>{ 'Gender' }</label>
-          <Input type="radio" label="Male" value="Male" />
+          <Input type="radio" label="Male" value="Male" active />
           <Input type="radio" label="Female" value="Female" />
         </RadioField>
         { this.displayErrors('gender') }
 
         <RadioField model="survey.hispanic">
           <label>{ 'Are you of Hispanic, Latino, or Spanish origin?' }</label>
-          <Input type="radio" label="No" value="No" />
+          <Input type="radio" label="No" value="No" active />
           <Input type="radio" label="Yes, Mexican, Mexican-American, or Chicano" value="Yes, Mexican, Mexican-American, or Chicano" />
           <Input type="radio" label="Yes, Puerto Rican" value="Yes, Puerto Rican" />
           <Input type="radio" label="Yes, Cuban" value="Yes, Cuban" />
@@ -58,31 +59,31 @@ export default class SurveyPage1 extends React.Component {
         { this.displayErrors('hispanic') }
 
         <label>{ 'What is your race? Check all that apply' }</label>
-        <Input onChange={ this.handleChange('white') } type="checkbox" label="White" />
-        <Input onChange={ this.handleChange('blackOrAfricanAmerican') } type="checkbox" label="Black or African American" />
-        <Input onChange={ this.handleChange('americanIndianOrAlaskaNative') } type="checkbox" label="American Indian or Alaska Native" />
-        <Input onChange={ this.handleChange('asianIndian') } type="checkbox" label="Asian Indian" />
-        <Input onChange={ this.handleChange('chinese') } type="checkbox" label="Chinese" />
-        <Input onChange={ this.handleChange('filipino') } type="checkbox" label="Filipino" />
-        <Input onChange={ this.handleChange('japanese') } type="checkbox" label="Japanese" />
-        <Input onChange={ this.handleChange('korean') } type="checkbox" label="Korean" />
-        <Input onChange={ this.handleChange('vietnamese') } type="checkbox" label="Vietnamese" />
-        <Input onChange={ this.handleChange('otherAsian') } type="checkbox" label="Other Asian" />
-        <Input onChange={ this.handleChange('pacificIslander') } type="checkbox" label="Pacific Islander" />
+        <Input onChange={ this.handleChange('white') } type="checkbox" label="White" checked={ race.white } />
+        <Input onChange={ this.handleChange('blackOrAfricanAmerican') } type="checkbox" label="Black or African American" checked={ race.blackOrAfricanAmerican } />
+        <Input onChange={ this.handleChange('americanIndianOrAlaskaNative') } type="checkbox" label="American Indian or Alaska Native" checked={ race.americanIndianOrAlaskaNative } />
+        <Input onChange={ this.handleChange('asianIndian') } type="checkbox" label="Asian Indian" checked={ race.asianIndian } />
+        <Input onChange={ this.handleChange('chinese') } type="checkbox" label="Chinese" checked={ race.chinese } />
+        <Input onChange={ this.handleChange('filipino') } type="checkbox" label="Filipino" checked={ race.filipino } />
+        <Input onChange={ this.handleChange('japanese') } type="checkbox" label="Japanese" checked={ race.japanese } />
+        <Input onChange={ this.handleChange('korean') } type="checkbox" label="Korean" checked={ race.korean } />
+        <Input onChange={ this.handleChange('vietnamese') } type="checkbox" label="Vietnamese" checked={ race.vietnamese } />
+        <Input onChange={ this.handleChange('otherAsian') } type="checkbox" label="Other Asian" checked={ race.otherAsian } />
+        <Input onChange={ this.handleChange('pacificIslander') } type="checkbox" label="Pacific Islander" checked={ race.pacificIslander } />
         <TextField model="survey.race.other">
-          <Input type="text" label="Other Race (Fill in)" placeholder="Other Race" />
+          <Input type="text" label="Other Race (Fill in)" placeholder="Other Race" value={ race.other } />
         </TextField>
 
         <RadioField model="survey.isBornInUS">
           <label>{ 'Were you born in the United States?' }</label>
-          <Input type="radio" label="Yes" value="Yes" />
+          <Input type="radio" label="Yes" value="Yes" active />
           <Input type="radio" label="No" value="No" />
         </RadioField>
         { this.displayErrors('isBornInUS') }
 
         <RadioField model="survey.isEnglishPrimaryLanguage">
           <label>{ 'Is English the primary language spoken at home?' }</label>
-          <Input type="radio" label="Yes" value="Yes" />
+          <Input type="radio" label="Yes" value="Yes" active />
           <Input type="radio" label="No" value="No" />
         </RadioField>
         { this.displayErrors('isEnglishPrimaryLanguage') }
