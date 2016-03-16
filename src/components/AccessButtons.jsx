9a0ec92a1901d482d03a4eeee4142af7e7a4cd22
Nick <nicholas.zarate@gmail.com>
2016-03-16 17:40:13 -0400

    Add 'clear cache' button that removes localstorage from the browser window, (for development)

--- src/components/AccessButtons.jsx
+++ src/components/AccessButtons.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default class Bins extends React.Component {
+  handleClick = () => window.localStorage.clear();
+
   render() {
     return (
       <div>
         <button onClick={ this.props.onLogin }>{ 'Log In' }</button>
         <Link to={ '/signup' }><button>{ 'Sign Up' }</button></Link>
+        <button onClick={ this.handleClick }>{ 'Clear Cache!!' }</button>
       </div>
     )
   }
