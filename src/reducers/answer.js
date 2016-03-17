b8ebdcc9a13b5dee27d689cc8a892473e3d52b22
Nick <nicholas.zarate@gmail.com>
2016-03-17 15:56:18 -0400

    Change answer reducer to answer DUCK

--- src/reducers/answer.js
+++ src/reducers/answer.js
@@ -1,24 +1,38 @@
-import { DEPOSIT, WITHDRAW, SET_BIN_VALUES, SET_BANK, SET_ANSWER_SUBMITTED } from 'actions/constants'
 import update from 'react-addons-update'
+import { createAction } from 'redux-actions'
 
+/* Action Types */
+const DEPOSIT = 'answer/DEPOSIT'
+const WITHDRAW = 'answer/WITHDRAW'
+const SET_BIN_VALUES = 'answer/SET_BIN_VALUES'
+const SET_BANK = 'answer/SET_BANK'
+const SET_ANSWER_SUBMITTED = 'answer/SET_ANSWER_SUBMITTED'
+
+/* Action Creators */
+export const deposit = createAction(DEPOSIT, (binsIndex, index) => { binsIndex, index })
+export const setAnswerSubmitted = createAction(SET_ANSWER_SUBMITTED, (submitted) => { submitted })
+export const setBank = createAction(SET_BANK, (bank) => { bank })
+export const setBinValues = createAction(SET_BIN_VALUES, (binValues) => { binValues })
+export const withdraw = createAction(WITHDRAW, (bankIndex) => { bankIndex })
+
+/* Reducer */
 const initialState = {
   bank: [],
   binValues: [],
   submitted: true
 }
-
 export default function answer(state = initialState, action) {
   switch (action.type) {
   case DEPOSIT:
     return update(state, {binValues: {[action.payload.binsIndex]: {[action.payload.index]: {$apply: (value) => value + 1}}}})
   case SET_ANSWER_SUBMITTED:
-    return update(state, {submitted: {$set: action.payload}})
+    return update(state, {submitted: {$set: action.payload.submitted}})
   case SET_BANK:
-    return update(state, {bank: {$set: action.payload}})
+    return update(state, {bank: {$set: action.payload.bank}})
   case SET_BIN_VALUES:
-    return update(state, {binValues: {$set: action.payload}})
+    return update(state, {binValues: {$set: action.payload.binValues}})
   case WITHDRAW:
-    return update(state, {bank: {[action.payload]: {$apply: (bank) => bank - 1}}})
+    return update(state, {bank: {[action.payload.bankIndex]: {$apply: (bank) => bank - 1}}})
   default:
     return state
   }
