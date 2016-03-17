be2501afbc767d2c05221b91ce82eda178bf2c07
Nick <nicholas.zarate@gmail.com>
2016-03-17 15:56:45 -0400

    Set tooltip reducer to tooltip DUCK

--- src/reducers/tooltip.js
+++ src/reducers/tooltip.js
@@ -1,19 +1,29 @@
-import { RESET_TOOLTIP, SET_TOOLTIP_TARGET, SET_TOOLTIP_MESSAGE } from 'actions/constants'
 import update from 'react-addons-update'
+import { createAction } from 'redux-actions'
 
+/* ACTION TYPES */
+const RESET_TOOLTIP = 'tooltip/RESET_TOOLTIP'
+const SET_TOOLTIP_MESSAGE = 'tooltip/SET_TOOLTIP_MESSAGE'
+const SET_TOOLTIP_TARGET = 'tooltip/SET_TOOLTIP_TARGET'
+
+/* ACTIONS */
+export const resetTooltip = createAction(RESET_TOOLTIP)
+export const setTooltipMessage = createAction(SET_TOOLTIP_MESSAGE, (message) => { message })
+export const setTooltipTarget = createAction(SET_TOOLTIP_TARGET, (target) => { target })
+
+/* REDUCER */
 const initialState = {
   target: '',
   message: ''
 }
-
 export default function tooltip(state = initialState, action) {
   switch (action.type) {
   case RESET_TOOLTIP:
     return update(state, {$set: initialState})
   case SET_TOOLTIP_MESSAGE:
-    return update(state, {message: {$set: action.payload}})
+    return update(state, {message: {$set: action.payload.message}})
   case SET_TOOLTIP_TARGET:
-    return update(state, {target: {$set: action.payload}})
+    return update(state, {target: {$set: action.payload.target}})
   default:
     return state
   }
