f7f121bd7dd5e9d101e7607cd0a410a9139fcfbd
Nick <nicholas.zarate@gmail.com>
2016-03-21 09:17:43 -0400

    Rename reduxify connect, and allow the user to pass in a reducer (or state) of their choosing if a selector is not to be added

---
+++ src/store/connect.js
@@ -0,0 +1,34 @@
+import { bindActionCreators } from 'redux'
+import { connect as rrConnect } from 'react-redux'
+
+/*
+ * Connects a react component to the redux store.
+ *
+ * @param state      - The name of the desired substate.
+ * @param selector   - a memoized selector function describing selection logic. The selector
+ *                     efficiently computes derived data from the store.
+ * @param actions    - The actions that will be provided to the connected component.
+ *
+ * @return           - If the container was specified, the connected container.
+ *                     Otherwise, the enhancer function.
+ */
+export default function connect({ state, selector, actions }) {
+  /* the container will subscribe to Redux store updates */
+  if (state) {
+    var mapStateToProps = (STATE) => selector ?
+      { [state]: selector(STATE) }
+    : { [state]: STATE[state] }
+  }
+
+  /* the container will be provided actions */
+  if (actions) {
+    var mapDispatchToProps = (dispatch) => ({
+      actions: bindActionCreators(actions, dispatch)
+    })
+  }
+
+  return rrConnect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+}
