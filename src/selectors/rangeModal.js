5b45917e15effdb4c3742d3194e2a872825023d2
Nick <nicholas.zarate@gmail.com>
2016-03-25 17:35:22 -0400

    Put in custom round instructions to be displayed to the user

--- src/selectors/rangeModal.js
+++ src/selectors/rangeModal.js
@@ -3,15 +3,18 @@ import { createSelector } from 'reselect'
 const showModalSelector = (state) => state.modal.showModal
 const categorySelector = (state) => state.category
 const unlockedSelector = (state) => state.round.unlocked
+const roundInstructionsSelector = (state) => state.category.roundInstructions
 
 export default createSelector(
   showModalSelector,
   categorySelector,
   unlockedSelector,
-  (showModal, category, unlocked) => {
+  roundInstructionsSelector,
+  (showModal, category, unlocked, roundInstructions) => {
     return {
       showModal,
-      unlocked: unlocked[category.index]
+      unlocked: unlocked[category.index],
+      roundInstructions
     }
   }
 )
