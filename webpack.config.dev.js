6a2f229ebeeb4ec0bb9ecb40b12991e61c2f63a9
Jonathan Sun <sunyang713@users.noreply.github.com>
2016-03-24 21:37:12 -0400

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -33,9 +33,7 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve("dist"),
     pathinfo: true,
-    publicPath: "/",
-    // sourceMapFilename must be named 'index' for sourcemapping to work. fucking stupid.
-    sourceMapFilename: "index.js.map"
+    publicPath: "/"
   },
   module: {
     loaders: [
