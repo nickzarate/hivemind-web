94cc13672b5530b262acdb3359dcaee20d943609
Nick <nicholas.zarate@gmail.com>
2016-03-26 23:46:43 -0400

    Add number field for rendering a 'number' type field

---
+++ src/components/NumberField.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Field } from 'react-redux-form'
+import { toNum } from 'toolbox/parser'
+
+export default class NumberField extends React.Component {
+  render() {
+    const { model, variableName } = this.props
+    return (
+      <Field
+        model={ model }
+        ref={ (ref) => this[variableName] = ref }
+        parser={ toNum }
+      >
+        <input
+          type="number"
+          placeholder={ variableName }
+        />
+      </Field>
+    )
+  }
+}
